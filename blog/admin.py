0375eacaa588375ffdd5e3e1b6be7ad44f5e3795
Johannes Ferreira <johannesssf@gmail.com>
2020-07-26 19:38:12 -0300

    Add blog app

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Post, Category
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- personal_site/settings.py
+++ personal_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'personal_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['personal_site/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

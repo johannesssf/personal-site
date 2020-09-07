d5b5fd8c6220a3c60352f53cc5b3451cf33ae53a
Johannes Ferreira <johannesssf@gmail.com>
2020-09-07 15:57:48 -0300

    Creating user accounts

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', include('projects.urls')),
+    path('', include('projects.urls')),
     path('blog/', include('blog.urls')),
+    path('users/', include('users.urls')),
 ]
